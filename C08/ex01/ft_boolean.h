434e766be019cb19f3ce4bbeade00b08f55bd8ba
Houssam-eddine Drabi <hdrabi@e2r7p10.1337.ma>
2021-07-11 18:54:37 +0100

    1337 pool

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdrabi <hdrabi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/11 18:45:41 by hdrabi            #+#    #+#             */
+/*   Updated: 2021/07/11 18:53:06 by hdrabi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+#define FT_BOOLEAN_H
+#include <unistd.h>
+
+typedef int t_bool;
+#define EVEN(Value) Value % 2 == 0
+#define TRUE 1
+#define FALSE 0
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
+#define SUCCESS 0
+
+#endif
