1936f03314c00813a43175105dacf9d800014c95
Houssam-eddine Drabi <hdrabi@e2r7p12.1337.ma>
2021-07-13 15:05:26 +0100

    1337 pool

--- C09/ex01/includes/ft.h
+++ C09/ex01/includes/ft.h
@@ -6,18 +6,18 @@
 /*   By: hdrabi <hdrabi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 13:21:19 by hdrabi            #+#    #+#             */
-/*   Updated: 2021/07/13 13:22:40 by hdrabi           ###   ########.fr       */
+/*   Updated: 2021/07/13 15:05:21 by hdrabi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
-#define FT_H
-#include <unistd.h>
+# define FT_H
+# include <unistd.h>
 
-void ft_putchar(char c);
-void ft_putstr(char *str);
-int ft_strcmp(char *s1, char *s2);
-int ft_strlen(char *str);
-void ft_swap(int *a, int *b);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
 
 #endif
